79d45da8bed0b3ba9b0f03c2060731586a948dcb
Clint Anderson <55559910+clinta715@users.noreply.github.com>
2024-08-18 23:27:07 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# archive_viewer.py
-import tkinter as tk
-from tkinter import ttk, filedialog, messagebox
-import os
-import threading
-
-from .ui_components import create_widgets, set_dark_theme
-from .archive_handlers import populate_tree, extract_files, get_archive_handler
-from .utils import SUPPORTED_FORMATS
